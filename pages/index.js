fa721d5e6494128514aaab274d654efb341906c0
SeungHwan Hong <mitzbarbie@naver.com>
2022-10-15 18:20:40 +0900

    Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
 export default function Home() {
-  return "Home test!!!!";
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>hallo {counter}</h1>
+      <button onClick={() => setCounter((add) => add + 1)}>+</button>
+      <button onClick={() => setCounter((min) => min - 1)}>-</button>
+    </div>
+  );
 }
