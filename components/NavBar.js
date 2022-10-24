1d5ce17a2a89e975ce20e11bea7cb5920177e030
SeungHwan Hong <mitzbarbie@naver.com>
2022-10-24 17:49:36 +0900

    add : comment

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,29 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
-    const router = useRouter();
-    console.log(router)
-    // method 1 and 2 to make styles
-    return (
+  const router = useRouter();
+  console.log(router);
+  // method 1 and 2 to make styles and jsx
+  return (
     <nav>
-        <Link href='/'>
-            <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-        </Link>
-        <Link href='/about'>
-            <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-        </Link>
-        <style jsx>{`
-            nav {
-                background-color : yellow;
-            }
-            a {
-                text-decoration : none;
-            }
-            .active {
-                color : red;
-            }
-        `}</style>
+      <Link href="/">
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+      </Link>
+      <style jsx>{`
+        nav {
+          background-color: yellow;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: red;
+        }
+      `}</style>
     </nav>
-    );
+  );
 }
