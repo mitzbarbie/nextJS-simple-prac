bf1d717e9529da3bb75551777ae7a6a6f6659481
Seung Hwan Hong (office) <mitzbarbie@naver.com>
2022-10-25 17:05:27 +0900

    global css and setting for common

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,8 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar/>
       <h1>hallo This is Home!!!!!!! ahahahah</h1>
       <a>test</a>
-      <style jsx>{`
-        a {
-          color : purple;
-          background-color : skyblue;
-        }
-      `}</style>
     </div>
   );
 }
