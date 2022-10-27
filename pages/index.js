ba8b3380ef57656161cc6f07a53757d2796a7b7f
Seung Hwan Hong (office) <mitzbarbie@naver.com>
2022-10-27 18:33:18 +0900

    prettier setting

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react';
 import Title from '../components/Title';
 
 const API_KEY = '10923b261ba94d897ac6b81148314a3f';
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, [])
   return (
     <div>
       <Title title='Home' />
-      <h1>hallo This is Home!!!!!!! ahahahah</h1>
-      <a>test</a>
+      {movies.map(movie=><div key={movie.id}></div>)}
     </div>
   );
 }
